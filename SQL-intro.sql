da51328c338d42a4e365d0601aaf0ff5162ced79
HaydenWashburn <hayden.washburn1@gmail.com>
2023-04-04 14:25:05 -0500

    changed = null to IS Null on lines 16 and 28

--- SQL-intro.sql
+++ SQL-intro.sql
@@ -13,7 +13,7 @@ SELECT * FROM products WHERE NOT price = 11.99;
 -- find all products and sort them by price from greatest to least
 SELECT * FROM products ORDER BY price DESC;
 -- find all employees who don't have a middle initial
-SELECT * FROM employees WHERE MiddleInitial = null;
+SELECT * FROM employees WHERE MiddleInitial IS NULL;
 -- find distinct product prices
 SELECT distinct products.price FROM products;
 -- find all employees whose first name starts with the letter ‘j’
@@ -25,6 +25,6 @@ SELECT * FROM products WHERE OnSale = 1;
 -- find the average price of all products
 SELECT AVG(products.price) FROM products;
 -- find all Geek Squad employees who don't have a middle initial
-SELECT * FROM employees WHERE title = 'Geek Squad';
+SELECT * FROM employees WHERE MiddleInitial IS Null AND title = 'Geek Squad';
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
 SELECT * FROM products WHERE StockLevel BETWEEN 500 AND 1200 ORDER BY price;
